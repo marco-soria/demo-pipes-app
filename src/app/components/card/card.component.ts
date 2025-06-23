c8f76a437d5117a9dcfd98c87bcee5ea43dbc3c2
marco-soria <marco.soriaa7@gmail.com>
2025-06-23 12:09:54 -0500

    added uncommon pipes

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
 })
 export class CardComponent {
-
+  title = input.required();
 }
