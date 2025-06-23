38a8a372fbce3a553e94b53bbcf6c945a8d47e97
marco-soria <marco.soriaa7@gmail.com>
2025-06-23 14:29:45 -0500

    updated validation in local service

--- src/app/services/locale.service.ts
+++ src/app/services/locale.service.ts
@@ -6,15 +6,27 @@ export type AvailableLocale = 'es' | 'fr' | 'en';
 export class LocaleService {
   private currentLocale = signal<AvailableLocale>('fr');
 
+  // constructor() {
+  //   this.currentLocale.set(
+  //     (localStorage.getItem('locale') as AvailableLocale) ?? 'es'
+  //   );
+  // }
+  // Validación explícita: Ahora obtenemos el valor del localStorage, lo validamos con el método isValidLocale(), y solo si es válido lo usamos
+  // Valor por defecto seguro: Si el valor no es válido o es null, usamos 'es' como valor por defecto
+  // El método isValidLocale() es un type guard que:
+
+  // Verifica que el valor no sea null
+  // Verifica que el valor esté incluido en el array de locales válidos
+  // Retorna locale is AvailableLocale que le dice a TypeScript que si la función retorna true, el parámetro es definitivamente del tipo AvailableLocale
+  // Esta aproximación es mucho más segura porque garantiza que solo valores válidos se asignen a currentLocale, evitando posibles errores en tiempo de ejecución si alguien modifica manualmente el localStorage con un valor inválido.
   constructor() {
-    this.currentLocale.set(
-      (localStorage.getItem('locale') as AvailableLocale) ?? 'es'
-    );
-    //validating the locale from localStorage before setting it
-    // const storedLocale = localStorage.getItem('locale') as AvailableLocale;
-    // if (storedLocale && ['es', 'fr', 'en'].includes(storedLocale)) {
-    //   this.currentLocale.set(storedLocale);
-    // }
+    const storedLocale = localStorage.getItem('locale');
+    const validLocale = this.isValidLocale(storedLocale) ? storedLocale : 'fr';
+    this.currentLocale.set(validLocale);
+  }
+
+  private isValidLocale(locale: string | null): locale is AvailableLocale {
+    return locale !== null && ['es', 'fr', 'en'].includes(locale);
   }
 
   get getLocale() {
